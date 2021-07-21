dcf2e166da26a5a134d3e131655a10f914dfc1b4
Keviindran Ramachandran <keviinx@yahoo.com>
2021-07-21 22:10:03 +0200

    done until timestamp 55:09

--- src/strings.rs
+++ src/strings.rs
@@ -19,5 +19,27 @@ pub fn run(){
     // Check is empty
     println!("Is Empty: {}", hello.is_empty());
 
+    // Contains
+    println!("Contains 'World' {}", hello.contains("World"));
+
+    // Replace
+    println!("Replace: {}", hello.replace("World", "There"));
+
+    // Loop through string by whitespace
+    for word in hello.split_whitespace(){
+        println!("{}", word);
+    }
+
+    // Create string with capacity
+    let mut s = String::with_capacity(10);
+    s.push('a');
+    s.push('b');
+
+    // Assertion testing
+    assert_eq!(2, s.len());
+    assert_eq!(10, s.capacity());
+
+    println!("{}", s);
+
     println!("{}", hello);
 }
